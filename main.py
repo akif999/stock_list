1f32b0f605cd2c69c8d681801ad41d84f174e8e0
akif999 <aki01nf28@gmail.com>
2025-08-03 23:29:25 +0900

    add samples of pandas lib

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from pprint import pprint
-import yfinance as yf
+# import yfinance as yf
 import pandas as pd
 
 
@@ -20,15 +20,37 @@ def main():
         "./testdata/20250301_investment.xlsx",
         sheet_name="四季報"
     )
-    private_stock_infos = df[["code", "会社名"]]
-    pprint(private_stock_infos)
+    df_private = df[["code", "会社名"]]
+    print('df_private')
+    pprint(df_private)
     # get jpx list
     df = pd.read_excel(
         "./testdata/data_j.xls",
         sheet_name="Sheet1"
     )
-    jpx_stock_infos = df[["コード", "銘柄名"]]
-    pprint(jpx_stock_infos)
+    df_jpxs = df[["コード", "銘柄名"]]
+    print('df_jpxs')
+    pprint(df_jpxs)
+
+    private_stock_infos = df_private[['code', '会社名']].copy()
+    private_stock_infos['code'] = private_stock_infos['code'].astype(
+        str).str.strip().str.zfill(4)
+    jpx_stock_infos = df_jpxs[['コード', '銘柄名']].copy()
+    jpx_stock_infos['コード'] = jpx_stock_infos['コード'].astype(
+        str).str.strip().str.zfill(4)
+    diff = jpx_stock_infos[~jpx_stock_infos['コード'].isin(
+        private_stock_infos['code']
+    )]
+    print('diff')
+    pprint(diff)
+
+    # not_matched = jpx_stock_infos[~jpx_stock_infos['コード'].isin(
+    #     private_stock_infos['code']
+    # )]['コード']
+    # pprint(not_matched.tolist())
+
+    # print(private_stock_infos.index)
+    # print(jpx_stock_infos.index)
 
 
 if __name__ == '__main__':
