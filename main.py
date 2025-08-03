6e6b7dbe2a08ac289ade93592590245c1dd9e701
akif999 <aki01nf28@gmail.com>
2025-08-03 17:03:42 +0900

    add sample of pandas lib

--- main.py
+++ main.py
@@ -1,12 +1,34 @@
+from pprint import pprint
 import yfinance as yf
+import pandas as pd
 
 
 def main():
-    print('xxx')
+    # print('xxx')
     # yfinance sample request
-    t = yf.Ticker("7203.T")
-    df = t.history(period="1mo")
-    print(df.head())
+    # t = yf.Ticker("7203.T")
+    # pprint(t.info)
+    # h = t.history(period="1mo")
+    # print(h.head())
+
+    # extract list data
+    # private list
+    pd.set_option('display.max_rows', None)
+    pd.set_option('display.max_columns', None)
+    # get private list
+    df = pd.read_excel(
+        "./testdata/20250301_investment.xlsx",
+        sheet_name="四季報"
+    )
+    private_stock_infos = df[["code", "会社名"]]
+    pprint(private_stock_infos)
+    # get jpx list
+    df = pd.read_excel(
+        "./testdata/data_j.xls",
+        sheet_name="Sheet1"
+    )
+    jpx_stock_infos = df[["コード", "銘柄名"]]
+    pprint(jpx_stock_infos)
 
 
 if __name__ == '__main__':
