60a57f8f1e78f52f2d65cbb02d0ff4fbe885e324
akif999 <aki01nf28@gmail.com>
2025-08-03 16:17:55 +0900

    add sample of yfinance lib

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
+import yfinance as yf
+
+
 def main():
     print('xxx')
+    # yfinance sample request
+    t = yf.Ticker("7203.T")
+    df = t.history(period="1mo")
+    print(df.head())
 
 
 if __name__ == '__main__':
