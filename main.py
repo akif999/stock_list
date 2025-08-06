8716a1436f899c7568d93e499afc877a7b2cfdda
akif999 <aki01nf28@gmail.com>
2025-08-06 11:12:32 +0900

    add process of merging tables

--- main.py
+++ main.py
@@ -21,17 +21,18 @@ def main():
         sheet_name="四季報"
     )
     df_private = df[["code", "会社名"]]
-    print('df_private')
-    pprint(df_private)
+    # print('df_private')
+    # pprint(df_private)
     # get jpx list
     df = pd.read_excel(
         "./testdata/data_j.xls",
         sheet_name="Sheet1"
     )
     df_jpxs = df[["コード", "銘柄名"]]
-    print('df_jpxs')
-    pprint(df_jpxs)
+    # print('df_jpxs')
+    # pprint(df_jpxs)
 
+    # diff codes
     private_stock_infos = df_private[['code', '会社名']].copy()
     private_stock_infos['code'] = private_stock_infos['code'].astype(
         str).str.strip().str.zfill(4)
@@ -41,8 +42,34 @@ def main():
     diff = jpx_stock_infos[~jpx_stock_infos['コード'].isin(
         private_stock_infos['code']
     )]
-    print('diff')
+    print('diff codes')
     pprint(diff)
+    print()
+
+    # merge names
+    print('merge names followig will be changed')
+    pprint(jpx_stock_infos)
+    merged_info = []
+    for i, p_row in private_stock_infos.iterrows():
+        info = {'code': p_row['code'], 'name': p_row['会社名']}
+        j_row = jpx_stock_infos[jpx_stock_infos['コード'] == p_row['code']]
+        if (not j_row.empty):
+            if j_row.iloc[0]['銘柄名'] != p_row['会社名']:
+                print(f"{p_row['code']}", end=': ')
+                print(f"{p_row['会社名']} -> {j_row.iloc[0]['銘柄名']}")
+                info['name'] = j_row.iloc[0]['銘柄名']
+            else:
+                pass
+                # print('names are same')
+        else:
+            pass
+            # print('code is nothing in jpx')
+        merged_info.append(info)
+
+    print()
+    print("merged names")
+    for info in merged_info:
+        print(f"{info['code']}\t{info['name']}")
 
     # not_matched = jpx_stock_infos[~jpx_stock_infos['コード'].isin(
     #     private_stock_infos['code']
